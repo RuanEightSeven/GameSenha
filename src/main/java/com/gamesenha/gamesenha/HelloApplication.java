f870268422d86a1ab399ccfb6b5532c868578d2c
Ruan Oliveira <tolentinoruan@gmail.com>
2023-09-07 00:56:05 -0300

    Adicionada a navegação entre telas e primeiras versões de modelo de telas

--- src/main/java/com/gamesenha/gamesenha/HelloApplication.java
+++ src/main/java/com/gamesenha/gamesenha/HelloApplication.java
@@ -2,19 +2,53 @@ package com.gamesenha.gamesenha;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
+    private static Stage stage;
+
+    private static Scene inicioScene;
+    private static Scene duplasScene;
+
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
+    public void start(Stage primaryStage) throws IOException {
+
+        stage = primaryStage;
+
+
+        stage.setTitle("inicio");
+
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Inicio.fxml"));
+
+        Parent iniciofxml = fxmlLoader.load(getClass().getResource("Inicio.fxml"));
+        inicioScene = new Scene(iniciofxml);
+
+        FXMLLoader fxmlduplas = new FXMLLoader(HelloApplication.class.getResource("TelaDuplas.fxml"));
+        Parent duplasfxml = fxmlduplas.load(getClass().getResource("TelaDuplas.fxml"));
+        duplasScene = new Scene(duplasfxml);
+
+
+        stage.setScene(inicioScene);
         stage.show();
+    }
+
+    public static void changeScreen(String scr){
+
+        switch (scr){
+            case "inicio":
+                stage.setScene(inicioScene);
+                stage.setTitle("inicio");
+                break;
+            case "duplas":
+                stage.setScene(duplasScene);
+                stage.setTitle("duplas");
+        }
+
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/com/gamesenha/gamesenha/InicioController.java
@@ -0,0 +1,21 @@
+package com.gamesenha.gamesenha;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+
+public class InicioController {
+
+    @FXML
+    private Button btnClick;
+
+    @FXML
+    protected void clicouBotao(ActionEvent event){
+
+        HelloApplication.changeScreen("duplas");
+        System.out.println("de fato");
+
+    }
+
+}
